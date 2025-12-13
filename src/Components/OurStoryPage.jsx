d909bb1003341b893a45148c7cc1aebdedaa586d
Israel AREMOH <israelaremoh@gmail.com>
2025-12-13 23:27:59 +0100

    updated OurstoryPage

--- src/Components/OurStoryPage.jsx
+++ src/Components/OurStoryPage.jsx
@@ -19,14 +19,14 @@ const OurStoryPage = () => {
       <div className="max-w-5xl mx-auto grid grid-rows-1 px-6 md:px-16 py-16">
         <div className="justify-self-start text-left max-w-xl">
           <blockquote className="text-xl md:text-2xl italic font-light">
-            Coming soon
+          I first met Faith when she accompanied someone on a visit to my mum, and I thought to myself, what a priceless gem we have here. Fast forward to years later, I looked her up on Facebook and messaged her. The rest, as they say, is history.
             <footer className="mt-4 text-sm text-[#6F4E37]">— Chukwudi</footer>
           </blockquote>
         </div>
 
         <div className="justify-self-end text-right max-w-xl mt-4 md:mt-24">
           <blockquote className="text-xl md:text-2xl italic font-light">
-            Coming Soon
+          Yes, it was true that I visited; he was really polite, respectful, and charmingly cute. We vibed really well when he contacted me on Facebook, and I'm happy to be sharing the rest of my life with him.
             <footer className="mt-4 text-sm text-[#6F4E37]">— Faith</footer>
           </blockquote>
         </div>
@@ -47,9 +47,14 @@ const OurStoryPage = () => {
           </div>
 
           <div className="relative z-10 -mt-10 lg:-ml-16 bg-[#9CAF88] text-white p-6 md:p-8 rounded-md shadow-xl w-full max-w-md">
-            <p className="mb-4 text-sm leading-relaxed">Coming Soon</p>
-            <p className="text-sm mb-4 leading-relaxed">Coming Soon</p>
-            <p className="text-sm mb-4">Coming Soon</p>
+            <p className="mb-4 text-sm leading-relaxed">I first noticed Faith a few years ago when she accompanied a friend to visit my mom. It wasn’t a formal introduction, but her beauty left a lasting impression on me.
+</p>
+            <p className="text-sm mb-4 leading-relaxed">Years later, I found her on Facebook and reached out. To my surprise, she was living in Lagos (VI), even though I had first seen her in Umuahia. We began chatting, and despite her being rarely active online, I felt drawn to her.
+            </p>
+            <p className="text-sm mb-4">Eventually, I asked her out, and we met at The Junction restaurant. I arrived early, and when she walked in, I was completely taken, she was even more beautiful than I remembered. We shared a simple meal of plantain and catfish, a small moment that marked the beginning of everything.
+
+            Three years later, that moment has led us here. Today, I am marrying the most beautiful woman in the world.
+            </p>
 
             <button className="bg-gray-900 hover:bg-[#B59410] transition px-4 py-2 rounded text-sm">
               <Link to="/moment">Moments</Link>
